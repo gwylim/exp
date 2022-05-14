48a0b59bf9b6524c06a450fcb9b725f6f1692026
Gwylim Ashley <gwylim.a@gmail.com>
2022-05-13 19:45:19 -0700

    Add command line argument parser

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,27 @@ mod value;
 use crate::interpreter::run;
 use crate::program::{compile, Expr};
 use crate::sexpr::Sexpr;
-use std::{env, fs};
+use clap::{Parser, Subcommand};
+use std::fs;
+
+#[derive(Parser)]
+#[clap(version = "0.1.0")]
+#[clap(propagate_version = true)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Run { file: std::path::PathBuf },
+}
 
 fn main() {
-    let input = fs::read_to_string(env::args().nth(1).expect("File not specified"))
-        .expect("Failed to read file");
+    let Args {
+        command: Command::Run { file },
+    } = Args::parse();
+    let input = fs::read_to_string(file).expect("Failed to read file");
     let sexpr: Sexpr = sexpr::sexpr_file(&input).expect("Failed to parse sexpr").1;
     let program: Expr = compile(&sexpr).expect("Syntax error");
     println!("{:#?}", run(&program).expect("Exception in execution"));
